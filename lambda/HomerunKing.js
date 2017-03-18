d2f26500cad7479ef2ecabba2291050fa202ceef
Dipesh Patel <dipesh@JiggyMomma.local>
2017-03-18 11:38:34 -0400

    App app utility.

--- lambda/HomerunKing.js
+++ lambda/HomerunKing.js
@@ -7,7 +7,10 @@ var async = require('async');
 
 var nconf = require('nconf') ;
 nconf.file({file: 'config.json'}) ;
-var dbutil = require('./my_modules/dbutil')(nconf.get('aws-config')) ;
+
+var awsConfig = nconf.get('aws-config') ;
+var apputilmod = require('./my_modules/apputil') ;
+var apputil = apputilmod(awsConfig) ;
 
 /*
  * Set start and end year with defaults 2015  2016
@@ -15,102 +18,24 @@ var dbutil = require('./my_modules/dbutil')(nconf.get('aws-config')) ;
 var startYear = (process.argv[2] || 2015)-0 ;
 var endYear = (process.argv[3] || 2016)-0 ;
 
-/*
- * Chain 1:  get home runs and playerID for given range
- */
-function homeruns(callback) {
-    var yrRange = [] ;
-    var n = endYear - startYear ; 
+(function() {	
+	
+	apputil.maxHomerunByYears(startYear, endYear, function(err, data) {
+		
+		if (err) {
+			console.log(err) ;
+		} else {
+	        for (var i = 0; i < data.length; i++) {
 
-    for (var i = 0; i < n; i++) {
-	yrRange.push(endYear - i) ;	
-    }
+	    	    var hr = data[i].HR ;
+	    	    var fullName = data[i].fullName ;
+	    	    var yearID = data[i].yearID ;
+	    	    var teamName = data[i].franchiseName ;
 
-    var results = [] ;
-    async.each(yrRange,
-        function(yr, cb) {
-
-            dbutil.topHomerunsByYear(yr, function(err, data) {
-                if (err) {
-                    console.error(err) ;
-                } else {
-		    if (data.Items.length > 0) {
-	                results.push(data.Items[0]) ;
-                    }
-                }
-                cb() ;
-            }) ;
-
-        }, function(err) {
-	    callback(null, results) ;
-	}
-    ) ;
-}
-
-/*
- * Chain 2:  for each playerID, lookup player name from Players table
- */
-function playerLookup(results, callback) {
-
-    async.each(results,
-        function(item, cb) {
-            dbutil.playerLookup(item.playerID, function(err, data) {
-                if (err) {
-                    console.error(err) ;
-                } else {
-                    item.fullName = data.Item.fullName ;
-                }
-            }) ;
-            cb() ;               
-        }, function(err) {
-            callback(null, results) ;
-        }
-    ) ;
-}
-
-
-/*
- * Chain 3:  function to lookup team name based on playerID and yearID 
- */
-function teamNameLookup(hr_items, callback) {
-    var r = {} ;
-    async.each(hr_items,
-        function(item, cb) {
-            dbutil.teamNameLookup(item.teamID, item.yearID, function(err, data) {
-                if (err) {
-                    console.error(err) ;
-                } else {
-                    item.franchiseName = data.Item.franchiseName ;
-                }
-                cb() ;
-            }) ;
-        }, function(err) {
-                callback(null, hr_items) ;
-        }
-    ) ;
-}
-
-
-/*
- * Calling function.  Chained to get homerun data for specific
- * range followed by player name lookup using playerID
- */
-(function() {
-    async.waterfall([ 
-        homeruns,
-        playerLookup,
-        teamNameLookup
-    ], function(error, nr) {
-
-	nr.sort(function(a,b) { return b.HR - a.HR ; } ) ;
-        for (var i = 0; i < nr.length; i++) {
-
-	    var hr = nr[i].HR ;
-	    var fullName = nr[i].fullName ;
-            var yearID = nr[i].yearID ;
-            var teamName = nr[i].franchiseName ;
-
-    	    console.log(hr + " -  " + fullName+ ", " + yearID + ", " + teamName ) ;
-        }
-    })
+	    	    console.log(hr + " -  " + fullName+ ", " + yearID + ", " + teamName ) ;	    	    
+	        }
+		}
+				
+	}) ;
+	
 })() ;
--- lambda/HomerunKingLambda.js
+++ lambda/HomerunKingLambda.js
@@ -1,88 +1,11 @@
 var async = require('async');
+
 var nconf = require('nconf') ;
 nconf.file({file: 'config.json'}) ;
-var dbutil = require('./my_modules/dbutil')(nconf.get('aws-config')) ;
 
-/*
- * Chain 1:  get home runs and playerID for given range
- */
-
-function homeruns(evt) {
-	return function(callback) {
-		
-		var startYear = (evt.startYear || 2015)-0 ;
-		var endYear = (evt.endYear || 2016)-0 ;
-
-		var yrRange = [] ;
-		var n = endYear - startYear ; 
-
-		for (var i = 0; i < n; i++) {
-			yrRange.push(endYear - i) ;	
-		}
-
-		var results = [] ;
-		async.each(yrRange, function(yr, cb) {
-
-			dbutil.topHomerunsByYear(yr, function(err, data) {
-				if (err) {
-					console.error(err) ;
-            	} else {
-            		if (data.Items.length > 0) {
-            			results.push(data.Items[0]) ;
-            		}
-            	}
-            	cb() ;
-            }) ;
-			
-		}, function(err) {
-			callback(null, evt, results) ;
-		}) ;
-	} ;
-}
-
-/*
- * Chain 2:  for each playerID, lookup player name from Players table
- */
-function playerLookup(evt, results, callback) {
-
-    async.each(results,
-        function(item, cb) {
-            dbutil.playerLookup(item.playerID, function(err, data) {
-                if (err) {
-                    console.error(err) ;
-                } else {
-                    item.fullName = data.Item.fullName ;
-                }
-            }) ;
-            cb() ;               
-        }, function(err) {
-            callback(null, evt, results) ;
-        }
-    ) ;
-}
-
-/*
- * Chain 3:  function to lookup team name based on playerID and yearID 
- */
-function teamNameLookup(evt, hr_items, callback) {
-    var r = {} ;
-    async.each(hr_items,
-        function(item, cb) {
-            dbutil.teamNameLookup(item.teamID, item.yearID, function(err, data) {
-                if (err) {
-                    console.error(err) ;
-                } else {
-                    item.franchiseName = data.Item.franchiseName ;
-                }
-                cb() ;
-            }) ;
-
-        }, function(err) {
-                callback(null, hr_items) ;
-        }
-    ) ;
-}
-
+var awsConfig = nconf.get('aws-config') ;
+var apputilmod = require('./my_modules/apputil') ;
+var apputil = apputilmod(awsConfig) ;
 
 /*
  *  Lambda handler for homerunking
@@ -90,24 +13,21 @@ function teamNameLookup(evt, hr_items, callback) {
 exports.handler = function(event, context, callback) {
 
     var evt = event ;
-    async.waterfall([ 
-        homeruns(evt),
-        playerLookup,
-        teamNameLookup
-    ], function(error, nr) {
+	apputil.maxHomerunByYears(evt.startYear, evt.endYear, function(err, data) {		
+		if (err) {
+		    callback(new Error(err));
+		} else {
+	        for (var i = 0; i < data.length; i++) {
 
-    	nr.sort(function(a,b) { return b.HR - a.HR ; } ) ;
-        for (var i = 0; i < nr.length; i++) {
+	    	    var hr = data[i].HR ;
+	    	    var fullName = data[i].fullName ;
+	    	    var yearID = data[i].yearID ;
+	    	    var teamName = data[i].franchiseName ;
 
-        	var hr = nr[i].HR ;
-        	var fullName = nr[i].fullName ;
-            var yearID = nr[i].yearID ;
-            var teamName = nr[i].franchiseName ;
-
-    	    console.log(hr + " -  " + fullName+ ", " + yearID + ", " + teamName ) ;
-        }
-        
-        callback(null, "Success") ;
-    })
-	
+	    	    console.log(hr + " -  " + fullName+ ", " + yearID + ", " + teamName ) ;	    	    
+	        }
+	        
+	        callback(null, "Success") ;
+		}				
+	}) ;	
 };
