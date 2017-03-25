a7dd0b1024800787de6e88359700f9ec5a178dcc
dipesh <dipesh@192.168.10.119>
2017-03-25 11:19:30 -0400

    Move test scripts to test folder.

---
+++ test/HomerunsSearch.js
@@ -0,0 +1,39 @@
+/*
+ *   node HomerunsSearch 1975 25
+ *   Get list of players with given HR for a given YEAR in HR order
+ */
+
+//  DB utility for homerns lookup
+var async = require('async');
+
+var nconf = require('nconf') ;
+nconf.file({file: 'config.json'}) ;
+
+var awsConfig = nconf.get('aws-config') ;
+var apputilmod = require('../lambda/my_modules/apputil') ;
+var apputil = apputilmod(awsConfig) ;
+
+/*
+ * Input with default of 2016 20 home runs
+ */
+var inpYear = (process.argv[2] || 2016)-0 ;
+var inpHR = (process.argv[3] || 20)-0 ;
+
+
+/*
+ * Calling function to get players with inputed home runs or more for inputed year
+ */
+(function() {
+
+	apputil.topHomerunsForYear(inpYear, inpHR, function(err, hr_items) {
+		if (err) {
+			console.log(err) ;
+		} else {
+			for (var i = 0; i < hr_items.length; i++) {
+				var data = hr_items[i] ;
+				console.log(data.HR + " -  " + data.fullName+ ", " + data.yearID + ", " + data.name) ;
+			}
+		}
+	}) ;
+
+})()
---
+++ test/PlayersSearch.js
@@ -0,0 +1,35 @@
+/*
+ *  node PlayersSearch Ortiz
+ *  Find player given last name
+ */
+
+var async = require('async');
+
+var nconf = require('nconf') ;
+nconf.file({file: 'config.json'}) ;
+
+var awsConfig = nconf.get('aws-config') ;
+var apputilmod = require('../lambda/my_modules/apputil') ;
+var apputil = apputilmod(awsConfig) ;
+
+
+/*
+ * Input last name
+ */
+var inpFirstname = (process.argv[2] || "David") ;
+var inpLastname = (process.argv[3] || "Ortiz") ;
+
+(function() {	
+
+	apputil.playerLookupByName(inpFirstname.toLowerCase(), inpLastname.toLowerCase(), function(err, data) {
+	    if (err) {
+	        console.error(err) ;
+	    } else {
+	    	for (var i = 0; i < data.length; i++ ) {
+	    	    var player = data[i] ;
+	            console.log(player) ;
+	    	}
+	    }
+	}) ;
+	
+})() ;
